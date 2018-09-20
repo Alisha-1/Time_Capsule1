9a50ed929c22f614824b84c68893e41d5c8aa1bd
Alisha-1 <41950561+Alisha-1@users.noreply.github.com>
2018-09-20 14:54:13 +0100

    Update setEnvVariables.sh

--- setEnvVariables.sh
+++ setEnvVariables.sh
@@ -1,7 +1,1 @@
-export PORT=8080
-export AWS_ACCESS_KEY_ID="AKIAJF5SZ64LKL5F3HEQ"
-export AWS_SECRET_ACCESS_KEY="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"
-export DATABASE_URL="postgres://femodzrnsqafnc:7e9011a13dc74d0ac54ac3776ebcea139197010fed339cc9f57686a16f5daf43@ec2-54-247-123-231.eu-west-1.compute.amazonaws.com:5432/d71aserb3ek998?ssl=true"
-export GMAIL_USER=shekhardreamforcedemos@gmail.com
-export GMAIL_PASSWORD=shepat9871
-export CAPSULE_URL="http://localhost:5000/#/Event/"
+
