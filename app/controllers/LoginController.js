63f9750f9ed2618ccb5782a6d5611539cfa813d0
Alisha-1 <41950561+Alisha-1@users.noreply.github.com>
2018-09-28 13:34:50 +0100

    Add files via upload

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -5,6 +5,30 @@ angular.module('myApp')
       password: ''
     }
 
+  //Google Login
+  $scope.glogin = function() {
+    debugger;
+    $http.get("url").then(function(response) {
+      windowOpened = window.open(response.data);
+    });
+  };
+
+  window.onmessage = function(e) {
+    windowOpened.close();
+    var urlWithCode = e.data;
+    var idx = urlWithCode.lastIndexOf("code=");
+    var code = urlWithCode.substring(idx + 5).replace("#", "");
+    var splitCode = code.split("&");
+    var oauthCode = splitCode[0];
+    $http({
+      url: "token",
+      method: "GET",
+      params: { code: oauthCode }
+    }).then(function(response) {
+      console.log(response);
+    });
+  };
+
     // $scope.gLogin = function(){
     //   AuthService.googlelogin()
     //   .then((user) => {
