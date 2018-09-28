2a9e61f4dbd4422139489812be56306662428ef2
Alisha-1 <41950561+Alisha-1@users.noreply.github.com>
2018-09-28 13:45:02 +0100

    Update mainController.js

--- app/mainController.js
+++ app/mainController.js
@@ -179,30 +179,4 @@ $scope.$on('$locationChangeSuccess', function(//EDIT: remove params for jshint )
     );
   };
 
-  
-  //Google Login
-  $scope.glogin = function() {
-    debugger;
-    $http.get("url").then(function(response) {
-      windowOpened = window.open(response.data);
-    });
-  };
-
-  window.onmessage = function(e) {
-    windowOpened.close();
-    var urlWithCode = e.data;
-    var idx = urlWithCode.lastIndexOf("code=");
-    var code = urlWithCode.substring(idx + 5).replace("#", "");
-    var splitCode = code.split("&");
-    var oauthCode = splitCode[0];
-    $http({
-      url: "token",
-      method: "GET",
-      params: { code: oauthCode }
-    }).then(function(response) {
-      console.log(response);
-    });
-  };
-
-
 });
