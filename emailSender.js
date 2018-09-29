01a8e6a308172ab12f85e38f9470a04b0650b3ce
Alisha-1 <41950561+Alisha-1@users.noreply.github.com>
2018-09-29 16:56:12 +0100

    Update emailSender.js

--- emailSender.js
+++ emailSender.js
@@ -59,7 +59,7 @@ async function run() {
     })
 
     // Update Sent flag on all records
-    await client.query('UPDATE "Time_Capsule-Recipient" SET Sent = true FROM (SELECT tc."CapsuleID", tc."Recieved_Date", tr."Recipient_Email" FROM "Time_Capsule" tc INNER JOIN "Time_Capsule-Recipient" tr ON tc."CapsuleID" = tr."CapsuleID" WHERE NOT tr.Sent AND tc."Recieved_Date" <= NOW()) ')
+    await client.query('UPDATE "Time_Capsule-Recipient" SET Sent = true FROM (SELECT tc."CapsuleID", tc."Recieved_Date", tr."Recipient_Email" FROM "Time_Capsule" tc INNER JOIN "Time_Capsule-Recipient" tr ON tc."CapsuleID" = tr."CapsuleID" WHERE NOT tr.Sent AND tc."Recieved_Date" <= NOW()) t')
 
     // Once the run is complete then insert job record
     await client.query(`INSERT INTO EMAIL_JOB (LAST_RUN) VALUES (NOW())`)
